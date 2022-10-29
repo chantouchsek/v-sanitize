6239dc5fb7131943739f8d561b345fdd4e1ffbb1
Patrik Janosdeak <patrik.janosdeak@isobar.com>
2022-10-29 14:52:06 +0200

    fix(ts): add proper shims for vue, so ts can resolve vue components

--- example/src/main.ts
+++ example/src/main.ts
@@ -2,8 +2,6 @@ import { createApp } from 'vue'
 import VSanitize from '../../src/sanitize'
 import App from '@/App.vue'
 
-const app = createApp(App).use(VSanitize, {
-
-})
+const app = createApp(App).use(VSanitize)
 
 app.mount('#app')
